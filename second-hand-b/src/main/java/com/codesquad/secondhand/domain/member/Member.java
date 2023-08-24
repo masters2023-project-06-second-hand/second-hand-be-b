e44bd37628e0f9c9c19ed14d450182bc2a81931f
jsjseop <57559288+jsjseop@users.noreply.github.com>
2023-08-24 11:24:15 +0900

    fix: Id GenerationType 변경
    Auto-increment 되도록 IDENTITY로 변경

--- second-hand-b/src/main/java/com/codesquad/secondhand/domain/member/Member.java
+++ second-hand-b/src/main/java/com/codesquad/secondhand/domain/member/Member.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String email;
--- second-hand-b/src/main/java/com/codesquad/secondhand/domain/product/Product.java
+++ second-hand-b/src/main/java/com/codesquad/secondhand/domain/product/Product.java
@@ -24,7 +24,7 @@ import lombok.NoArgsConstructor;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String name;
