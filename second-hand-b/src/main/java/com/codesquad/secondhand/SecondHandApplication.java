954b3cce990bb9f9ac06d23544bc3e55e5f5d48e
jsjseop <57559288+jsjseop@users.noreply.github.com>
2023-09-04 16:17:49 +0900

    refactor: sdk 에러 나지 않도록 설정 추가

--- second-hand-b/src/main/java/com/codesquad/secondhand/SecondHandApplication.java
+++ second-hand-b/src/main/java/com/codesquad/secondhand/SecondHandApplication.java
@@ -3,11 +3,18 @@ package com.codesquad.secondhand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        // 로컬 환경에서 aws sdk 에러 나지 않도록 설정
+        exclude = {
+                org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+                org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+                org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+        }
+)
 public class SecondHandApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecondHandApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandApplication.class, args);
+    }
 
 }
