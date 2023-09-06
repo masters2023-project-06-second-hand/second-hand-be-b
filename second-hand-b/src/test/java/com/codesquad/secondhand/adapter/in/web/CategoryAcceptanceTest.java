3aa7ebee8bc3b15d95b448abbd5b8ce7e317ca5b
jsjseop <57559288+jsjseop@users.noreply.github.com>
2023-09-06 21:34:52 +0900

    test: 테스트 메서드 분리

--- second-hand-b/src/test/java/com/codesquad/secondhand/adapter/in/web/CategoryAcceptanceTest.java
+++ second-hand-b/src/test/java/com/codesquad/secondhand/adapter/in/web/CategoryAcceptanceTest.java
@@ -1,15 +1,12 @@
 package com.codesquad.secondhand.adapter.in.web;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.codesquad.secondhand.adapter.in.web.CategorySteps.이미지포함_카테고리_목록조회를_검증한다;
+import static com.codesquad.secondhand.adapter.in.web.CategorySteps.카테고리_목록을_조회한다;
+import static com.codesquad.secondhand.adapter.in.web.CategorySteps.카테고리_목록조회를_검증한다;
 
 import com.codesquad.secondhand.utils.AcceptanceTest;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
 
 class CategoryAcceptanceTest extends AcceptanceTest {
 
@@ -17,36 +14,19 @@ class CategoryAcceptanceTest extends AcceptanceTest {
     @DisplayName("카테고리 목록 조회 요청을 받으면 카테고리 목록을 반환한다.")
     void getCategories() {
         //when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .queryParam("includeImages", false)
-                .auth().oauth2(ayaanAccessToken)
-                .when().get("/api/categories")
-                .then().log().all().extract();
+        var response = 카테고리_목록을_조회한다(false, ayaanAccessToken);
 
         //then
-        Assertions.assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(response.jsonPath().getList("id")).isNotEmpty(),
-                () -> assertThat(response.jsonPath().getList("name")).isNotEmpty()
-        );
+        카테고리_목록조회를_검증한다(response);
     }
 
     @Test
     @DisplayName("카테고리 목록 조회 요청을 받으면 includeImages가 true인 경우 imgUrl을 포함하는 카테고리 목록을 반환한다.")
     void getCategoriesWithImgUrl() {
         //when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .queryParam("includeImages", true)
-                .auth().oauth2(ayaanAccessToken)
-                .when().get("/api/categories")
-                .then().log().all().extract();
+        var response = 카테고리_목록을_조회한다(true, ayaanAccessToken);
 
         //then
-        Assertions.assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(response.jsonPath().getList("id")).isNotEmpty(),
-                () -> assertThat(response.jsonPath().getList("name")).isNotEmpty(),
-                () -> assertThat(response.jsonPath().getList("imgUrl")).isNotEmpty()
-        );
+        이미지포함_카테고리_목록조회를_검증한다(response);
     }
 }
