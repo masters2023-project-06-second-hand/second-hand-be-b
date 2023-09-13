3bd0faefa70f10764451e930d134aae4007071b8
ALbertIM0427 <guang7226961@naver.com>
2023-09-13 17:14:18 +0900

    test: ImagesIdValidator 테스트 구현

--- second-hand-b/src/main/java/com/codesquad/secondhand/adapter/in/web/request/annotation/ImagesIdValidator.java
+++ second-hand-b/src/main/java/com/codesquad/secondhand/adapter/in/web/request/annotation/ImagesIdValidator.java
@@ -6,12 +6,14 @@ import javax.validation.ConstraintValidatorContext;
 
 public class ImagesIdValidator implements ConstraintValidator<ImagesId, List<Long>> {
 
+    private static final int ZERO = 0;
+
     @Override
     public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
         if (values == null) {
             return true;
         }
-        return values.stream().allMatch(id -> id > 0);
+        return values.stream().allMatch(id -> id > ZERO);
     }
 }
 
