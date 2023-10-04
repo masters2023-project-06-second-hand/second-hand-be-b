a8734f39aab6e8e0c226f1588727a2e4b99e971e
jsjseop <57559288+jsjseop@users.noreply.github.com>
2023-10-04 23:28:44 +0900

    refactor: CrudRepository로 변경

--- second-hand-b/src/main/java/com/codesquad/secondhand/command/adapter/out/persistence/imports/ChatRoomCrudRepository.java
+++ second-hand-b/src/main/java/com/codesquad/secondhand/command/adapter/out/persistence/imports/ChatRoomCrudRepository.java
@@ -4,10 +4,10 @@ import com.codesquad.secondhand.command.domain.chat.ChatRoom;
 import com.codesquad.secondhand.command.domain.chat.dto.ChatRoomDto;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
-public interface ChatRoomCrudRepository extends JpaRepository<ChatRoom, Long> {
+public interface ChatRoomCrudRepository extends CrudRepository<ChatRoom, Long> {
 
     Optional<ChatRoom> findByProductIdAndSellerIdAndBuyerId(long productId, long sellerId, long buyerId);
 
